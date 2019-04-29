938d2912090c1a0b0e7169b1400ed564cb338c4e
saurav <saurav.srivastava@acelearningco.com>
2019-04-29 14:21:31 +0530

    second

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,55 @@
 var express = require('express');
 var router = express.Router();
 
+const request=require('request')
+const csv=require('csvtojson')
+const lodash = require('lodash');
+
+let onError = function(err) {
+	console.log(err)
+}
+
+let onComplete = function(data) {
+	console.log(data)
+}
+
+//var request = require('request');
+
+ 
+/* csv()
+.fromStream(request.get('https://docs.google.com/spreadsheets/d/157EMSMa84bQDX8RrqRXxCMe90ltMQzuaZ_SRSyWFDQM/edit?usp=sharing'))
+.subscribe((json)=>{
+    console.log(json)
+    return new Promise((resolve,reject)=>{
+        // long operation for each json e.g. transform / write into database.
+    })
+},onError,onComplete); */
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   //res.send('respond with a resource');
-	res.json({a:1});
+	request.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vRXMialXNW2fvJTtdfZeyMBaJGz50-fJYuQGPE-cGeXpFFhhl6j20C8mOmiIO6GjUO6E5PwIKqQGLOv/pub?gid=0&single=true&output=csv', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+		    var csv1 = body;
+		    // Continue with your processing here.
+		//console.log(csv1)
+	csv({
+		noheader:true,
+		output: "csv"
+	})
+	.fromString(csv1)
+	.then((csvRow)=>{ 
+		//var res = {data : {}}
+		res.json(csvRow)
+		//console.log(JSON.stringify(csvRow))
+		lodash.each(csvRow[0],(row)=>{
+		
+		})
+	
+	})
+    }
+});
+	//res.json({a:1});
 });
 
 module.exports = router;
