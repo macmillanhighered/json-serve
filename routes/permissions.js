dcf3511471675f4a071c8938f879d4f2cd07547f
saurav <saurav.srivastava@acelearningco.com>
2019-07-24 10:37:14 +0530

    permissions for survey

--- routes/permissions.js
+++ routes/permissions.js
@@ -3,7 +3,38 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.json({"data":[{"permission_id":248,"institute_id":"45e72a35-a596-42c2-842e-7745744974a9","institute_name":"Demo Institution","discipline_id":null,"discipline_name":null,"course_id":null,"course_name":null},{"permission_id":255,"institute_id":"45e72a35-a596-42c2-842e-7745744974a9","institute_name":"Demo Institution","discipline_id":null,"discipline_name":null,"course_id":null,"course_name":null},{"permission_id":256,"institute_id":"45e72a35-a596-42c2-842e-7745744974a9","institute_name":"Demo Institution","discipline_id":null,"discipline_name":null,"course_id":null,"course_name":null},{"permission_id":257,"institute_id":"45e72a35-a596-42c2-842e-7745744974a9","institute_name":"Demo Institution","discipline_id":null,"discipline_name":null,"course_id":null,"course_name":null},{"permission_id":258,"institute_id":"45e72a35-a596-42c2-842e-7745744974a9","institute_name":"Demo Institution","discipline_id":null,"discipline_name":null,"course_id":null,"course_name":null},{"permission_id":259,"institute_id":"45e72a35-a596-42c2-842e-7745744974a9","institute_name":"Demo Institution","discipline_id":null,"discipline_name":null,"course_id":null,"course_name":null},{"permission_id":260,"institute_id":"45e72a35-a596-42c2-842e-7745744974a9","institute_name":"Demo Institution","discipline_id":null,"discipline_name":null,"course_id":null,"course_name":null},{"permission_id":261,"institute_id":"45e72a35-a596-42c2-842e-7745744974a9","institute_name":"Demo Institution","discipline_id":null,"discipline_name":null,"course_id":null,"course_name":null}], "survey":{}})
+	res.json({"data":[{"permission_id":248,"institute_id":"45e72a35-a596-42c2-842e-7745744974a9","institute_name":"Demo Institution","discipline_id":null,"discipline_name":null,"course_id":null,"course_name":null},{"permission_id":255,"institute_id":"45e72a35-a596-42c2-842e-7745744974a9","institute_name":"Demo Institution","discipline_id":null,"discipline_name":null,"course_id":null,"course_name":null},{"permission_id":256,"institute_id":"45e72a35-a596-42c2-842e-7745744974a9","institute_name":"Demo Institution","discipline_id":null,"discipline_name":null,"course_id":null,"course_name":null},{"permission_id":257,"institute_id":"45e72a35-a596-42c2-842e-7745744974a9","institute_name":"Demo Institution","discipline_id":null,"discipline_name":null,"course_id":null,"course_name":null},{"permission_id":258,"institute_id":"45e72a35-a596-42c2-842e-7745744974a9","institute_name":"Demo Institution","discipline_id":null,"discipline_name":null,"course_id":null,"course_name":null},{"permission_id":259,"institute_id":"45e72a35-a596-42c2-842e-7745744974a9","institute_name":"Demo Institution","discipline_id":null,"discipline_name":null,"course_id":null,"course_name":null},{"permission_id":260,"institute_id":"45e72a35-a596-42c2-842e-7745744974a9","institute_name":"Demo Institution","discipline_id":null,"discipline_name":null,"course_id":null,"course_name":null},{"permission_id":261,"institute_id":"45e72a35-a596-42c2-842e-7745744974a9","institute_name":"Demo Institution","discipline_id":null,"discipline_name":null,"course_id":null,"course_name":null}], "survey":{  
+      "application_enabled":1,
+      "created_at":"2019-06-27 16:10:43",
+      "effective_at":null,
+      "email":"amruta+3@intelluslearning.com",
+      "expires_at":null,
+      "external_id":null,
+      "first_name":"Amruta",
+      "id":2,
+      "last_name":"+3",
+      "organizations":[  
+         {  
+            "display_name":"Demo University Survey",
+            "effective_at":null,
+            "expires_at":null,
+            "ipeds_id":null,
+            "organization_id":2,
+            "organization_name":"di0000000000006",
+            "salesforce_id":null
+         }
+      ],
+      "user_roles":[  
+         {  
+            "effective_at":null,
+            "expires_at":null,
+            "organization_id":2,
+            "organization_name":"di0000000000006",
+            "role_id":2,
+            "role_name":"admin"
+         }
+      ]
+   }})
 });
 
 module.exports = router;
